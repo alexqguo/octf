e0e165a99c20fddeebfffa048107c9f06de13b3c
Alex Guo <alexqguo@gmail.com>
2014-01-29 15:16:17 -0500

    add brief CSS

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -3,6 +3,7 @@ class AthletesController < ApplicationController
   before_filter :require_current_user!
   
   def index
+    @athletes = Athlete.all
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ AthleteLookup::Application.routes.draw do
   resource :session 
   resources :athletes
   
-  # root to: "static#index"
   root to: "sessions#new"
 end
