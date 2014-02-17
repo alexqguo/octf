0e4758518e235e5f4aaf11530dde2777a9b8029c
Alex Guo <alexqguo@gmail.com>
2014-02-16 20:04:09 -0500

    css tweak, remove jquery on athlete index page

--- app/assets/javascripts/highlighting.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	$(".athlete-list-el a").on("mouseover", function (event) {
-		$(this).toggleClass("selected");
-	});
-	
-	$(".athlete-list-el a").on("mouseout", function (event) {
-		$(this).toggleClass("selected");
-	});
-})
