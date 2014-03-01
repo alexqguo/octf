2b5304d3f9c87429afe3d90224070f4b18a888c0
Alex Guo <alexqguo@gmail.com>
2014-02-28 21:04:13 -0500

    add chosen to navbar

---
+++ app/assets/javascripts/chosen_form.js
@@ -0,0 +1,14 @@
+$(function () {
+	$(".chosen-select").chosen({
+		placeholder_text_single: "Find an athlete...",
+		width: "200px"
+	});
+	
+	$(".chosen-form").submit(function (event) {
+		event.preventDefault();
+		var athlete_id = $(".chosen-form select").val();
+		if (athlete_id) {
+			window.location.href = "/athletes/" + athlete_id;
+		}
+	});
+});
