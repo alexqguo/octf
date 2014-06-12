3b42a37c2316f0a1c42e7460a93799179992c07c
Alex Guo <alexqguo@gmail.com>
2014-06-11 23:53:39 -0400

    working on add videos, YouTubeIt bug

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,23 @@
 class VideosController < ApplicationController
+	before_filter :require_admin!, only: [:new, :create]
 
 	def index
 		@videos = Video.all
+	end
+
+	def new
+	end
+
+	def create
+		@video = Video.new(params[:video])
+
+		if @video.save
+			flash[:notice] = "Video successfully created!"
+			redirect_to videos_url
+		else
+			flash[:errors] = @video.errors.full_messages
+			redirect_to new_video_url
+		end
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ AthleteLookup::Application.routes.draw do
   get "admin", action: "admin", controller: "assets"
   post "send_email", action: "send_email", controller: "assets"
   resource :session, only: [:create, :destroy]
-  resources :videos, only: :index
+  resources :videos, only: [:index, :new, :create]
   resources :records, only: [:edit, :update]
   resources :athletes, except: :destroy do
     resources :marks, only: [:new, :create, :index, :update, :create, :destroy]
